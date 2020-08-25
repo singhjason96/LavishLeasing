776e6fe56629003c7ac0e70751d15a893d73189e
singhjason96 <jason.singh@baruchmail.cuny.edu>
2020-08-25 13:36:19 -0400

    package fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,10 +4,18 @@ import GradientBackground from "./GradientBackground";
 import NavBar from "./NavBar";
 import About from "./About";
 import TestimonialsSnippet from "./TestimonialsSnippet";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  bodyShift: {
+    position: 'fixed'
+  }
+}));
 
 const HomePage = () => {
+  const classes = useStyles();
   return (
-    <GradientBackground>
+    <GradientBackground className={classes.bodyShift}>
       <NavBar />
       <SectionOne />
       <About />
